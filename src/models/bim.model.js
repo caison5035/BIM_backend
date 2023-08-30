1d4d38e9735eb0154379dfc1f677fbd1132ef527
Caison Lee <caisonlee@autosnaplive.com>
2023-08-30 10:21:35 -0400

    Added name field

--- src/models/bim.model.js
+++ src/models/bim.model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const bimSchema = mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+    },
     url: {
       type: String,
       required: true,
